597209ca2038722460fff009eb6b558ad2375bb2
nadir35 <nadir_1@hotmail.de>
2014-05-25 21:08:45 +0200

    asdasdsad

--- leap/src/leap/Recognizer.java
+++ leap/src/leap/Recognizer.java
@@ -39,8 +39,6 @@ class SampleListener3 extends Listener {
 
 	public void onFrame(Controller controller) {
 		// Get the most recent frame and report some basic information
-		Frame frame = controller.frame();
-		Date now = new Date();
 
 	}
 
@@ -48,145 +46,92 @@ class SampleListener3 extends Listener {
 
 public class Recognizer {
 	public static Controller controller = new Controller();
-	public static SampleListener3 listener2 = new SampleListener3();
+	// private static SampleListener listener = new SampleListener();
 	public static float deviationX = 0.10F;
 	public static float deviationZ = 0.10F;
 	static Frame frame;
 
-	public static void main(String[] args) throws IOException,
-			InterruptedException {
-		// Create a sample listener and controller
-
-		// Have the sample listener receive events from the controller
-		controller.addListener(listener2);
-
-		// Remove the sample listener when done
-		// controller.removeListener(listener);
-
-		// TODO Auto-generated constructor stub
-	}
+	/*
+	 * public static void main(String[] args) throws IOException,
+	 * InterruptedException { // Create a sample listener and controller
+	 * 
+	 * // Have the sample listener receive events from the controller
+	 * controller.addListener(listener2);
+	 * 
+	 * // Remove the sample listener when done //
+	 * controller.removeListener(listener);
+	 * 
+	 * // TODO Auto-generated constructor stub }
+	 */
 
 	public static void recog(ArrayList<UserGesture> gestureList,
 			Controller controller) throws IOException, InterruptedException {
 		System.out.println("started recog");
 
-		/*
-		 * while(MapperGUI.statusRecognizing==true){ frame= controller.frame();
-		 * for (int i =0; i<gestureList.size();i++){ long timer=
-		 * System.currentTimeMillis();
-		 * //System.out.println("timer start"+timer); int matched = 0; for (int
-		 * j=1;j<gestureList.get(i).NodeList.size();j++){
-		 * while(System.currentTimeMillis()<(timer+2000)){
-		 * //System.out.println("timer inside"+timer); frame=
-		 * controller.frame();
-		 * if(frame.hands().get(0).palmPosition().normalized(
-		 * ).getX()>gestureList
-		 * .get(i).NodeList.get(j).frame.hands().get(0).palmPosition
-		 * ().normalized().getX()+deviationX ||
-		 * frame.hands().get(0).palmPosition
-		 * ().normalized().getX()<gestureList.get
-		 * (i).NodeList.get(j).frame.hands(
-		 * ).get(0).palmPosition().normalized().getX()-deviationX ||
-		 * frame.hands(
-		 * ).get(0).palmPosition().normalized().getZ()>gestureList.get
-		 * (i).NodeList
-		 * .get(j).frame.hands().get(0).palmPosition().normalized().getZ
-		 * ()+deviationZ ||
-		 * frame.hands().get(0).palmPosition().normalized().getZ
-		 * ()<gestureList.get
-		 * (i).NodeList.get(j).frame.hands().get(0).palmPosition
-		 * ().normalized().getZ()-deviationZ ){
-		 * //System.out.println(frame.hands(
-		 * ).get(0).palmPosition().normalized());
-		 * 
-		 * } else{ matched=matched+1; System.out.println("matched node number"
-		 * +j
-		 * +" "+gestureList.get(i).NodeList.get(j).frame.hands().get(0).palmPosition
-		 * (
-		 * ).normalized()+"with "+frame.hands().get(0).palmPosition().normalized
-		 * ()); timer= System.currentTimeMillis(); frame= controller.frame();
-		 * break;} } if(j!=matched){ System.out.println(
-		 * "RECOGNITION CRITICAL FAILURE: timelimit for next node was exceeded"
-		 * ); break;} else System.out.println("matcehd "+matched);
-		 * 
-		 * 
-		 * }
-		 * 
-		 * if(matched==gestureList.get(i).NodeList.size()-1){
-		 * System.out.println("RECOGNITION SUCCESS for gesture number"+i);
-		 * break;}
-		 * 
-		 * 
-		 * } MapperGUI.statusRecognizing=false;
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * }
-		 */
 		long[] timer = new long[gestureList.size()];
 		int[] matched = new int[gestureList.size()];
+
 		while (MapperGUI.statusRecognizing == true) {
 			frame = controller.frame();
 
-			for (int i = 0; i < gestureList.size(); i++) {
+			for (int gestureIterator = 0; gestureIterator < gestureList.size(); gestureIterator++) {
 				// System.out.println("timer start"+timer);
-				for (int j = matched[i] + 1; j < gestureList.get(i).NodeList
-						.size(); j++) {
-					if (System.currentTimeMillis() < (timer[i] + 2000)
-							|| timer[i] == 0) {
+				for (int nodeIterator = matched[gestureIterator] + 1; nodeIterator < gestureList
+						.get(gestureIterator).NodeList.size();) {
+					if (System.currentTimeMillis() < (timer[gestureIterator] + 2000)
+							|| timer[gestureIterator] == 0) {
 						// System.out.println("timer inside"+timer);
 						if (frame.hands().get(0).palmPosition().normalized()
-								.getX() < gestureList.get(i).NodeList.get(j).frame
-								.hands().get(0).palmPosition().normalized()
-								.getX()
+								.getX() < gestureList.get(gestureIterator).NodeList
+								.get(nodeIterator).frame.hands().get(0)
+								.palmPosition().normalized().getX()
 								+ deviationX
 								&& frame.hands().get(0).palmPosition()
-										.normalized().getX() > gestureList
-										.get(i).NodeList.get(j).frame.hands()
-										.get(0).palmPosition().normalized()
-										.getX()
-										- deviationX
+								.normalized().getX() > gestureList
+								.get(gestureIterator).NodeList
+								.get(nodeIterator).frame.hands().get(0)
+								.palmPosition().normalized().getX()
+								- deviationX
 								&& frame.hands().get(0).palmPosition()
-										.normalized().getZ() < gestureList
-										.get(i).NodeList.get(j).frame.hands()
-										.get(0).palmPosition().normalized()
-										.getZ()
-										+ deviationZ
+								.normalized().getZ() < gestureList
+								.get(gestureIterator).NodeList
+								.get(nodeIterator).frame.hands().get(0)
+								.palmPosition().normalized().getZ()
+								+ deviationZ
 								&& frame.hands().get(0).palmPosition()
-										.normalized().getZ() > gestureList
-										.get(i).NodeList.get(j).frame.hands()
-										.get(0).palmPosition().normalized()
-										.getZ()
-										- deviationZ) {
+								.normalized().getZ() > gestureList
+								.get(gestureIterator).NodeList
+								.get(nodeIterator).frame.hands().get(0)
+								.palmPosition().normalized().getZ()
+								- deviationZ) {
 							// System.out.println(frame.hands().get(0).palmPosition().normalized());
-							matched[i] = j;
-							timer[i] = System.currentTimeMillis();
+							matched[gestureIterator] = nodeIterator;
+							timer[gestureIterator] = System.currentTimeMillis();
 							System.out.println("matched node number "
-									+ j
+									+ nodeIterator
 									+ " of gesture "
-									+ i
+									+ gestureIterator
 									+ " with "
-									+ gestureList.get(i).NodeList.get(j).frame
-											.hands().get(0).palmPosition()
-											.normalized()
+									+ gestureList.get(gestureIterator).NodeList
+									.get(nodeIterator).frame.hands()
+									.get(0).palmPosition().normalized()
 									+ "with "
 									+ frame.hands().get(0).palmPosition()
-											.normalized());
+									.normalized());
 							break;
 						} else
 							break;
 					} else {
-						matched[i] = 0;
-						timer[i] = 0;
+						matched[gestureIterator] = 0;
+						timer[gestureIterator] = 0;
 						break;
 					}
 				}
-				if (matched[i] == gestureList.get(i).NodeList.size())
+				if (matched[gestureIterator] == gestureList
+						.get(gestureIterator).NodeList.size())
 					System.out
-							.println("RECOGNITION SUCCESS for gesture number "
-									+ i + " , action goes here");
+					.println("RECOGNITION SUCCESS for gesture number "
+							+ gestureIterator + " , action goes here");
 
 			}
 		}
